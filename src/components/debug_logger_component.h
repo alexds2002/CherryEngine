ab62b124e7c41c0da9b70a317ecc8a3f45125bba
alexds2002 <alexds2002@gmail.com>
2024-07-11 10:06:56 +0300

    [Glfw][fix] clean up code from Glfw libs and update documentation

--- src/components/debug_logger_component.h
+++ src/components/debug_logger_component.h
@@ -5,13 +5,11 @@
  * debug_logger_component is a collection of global variadic templated functions
  * with different overload options(color, time/date, and more to come).
  * The functionalities get compiled ONLY when DEBUG_MODE is defined in CMake,
- * otherwise the funcitons bodies are compiled empty to avoid Debug Logs in RELEASE_MODE.
- * debug_logger_component is planned to be a 'core' header that every class in the engine will have
- * so the debug colors will be visiable unless I put the file in a namespace.
+ * otherwise the funcitons bodies are compiled empty to avoid Debug Logs in RELEASE_MODE(optimization).
+ * TODO(Alex): debug_logger_component is planned to be a 'core' header that every class in the engine will have.
  *
  * !!! WARNINGS !!!
- * Debug functions SHOULD NOT be used for production purposes (RELEASE_MODE)
- * as they are only compiled in DEBUG_MODE -> CMAKE_BUILD_TYPE=Debug
+ * Shipped products(in RELEASE_MODE) should not rely on these functions as they are only compiled in DEBUG_MODE
  * However you do not need to delete them when shipping.
  * (BUG warning)inline global function with external linkage have undefined behavior
  * fix is to remove inline then the compilation will crash becouse of redefinition.
@@ -22,7 +20,7 @@
 
 #ifdef DEBUG_MODE
 #ifdef _WIN32 // 32 and 64
-#include <windows.h>
+#include <windows_definitions.h>
 #endif /* _WIN32 */
 #include <iostream>
 #include <chrono>
@@ -39,8 +37,7 @@ namespace DebugOnly
  * @brief Convert color to its coresponding windows API code
  *
  * Color converter for Windows systems
- * Only called when DEBUG_MODE is defined
- * RELEASE_MODE optimization
+ * Only compiled when DEBUG_MODE and _WIN32 is defined(RELEASE_MODE optimization)
  *
  * @param color: enum color to be converted
  *
@@ -108,9 +105,7 @@ unsigned char Windows_Color_To_Code(const EDebugColors color) noexcept
 /**
  * @brief Convert color to its coresponding ANSI code
  *
- * Color converter for Unix systems
- * Only called when DEBUG_MODE is defined
- * RELEASE_MODE optimization
+ * Color converter for Unix systems, compiled when DEBUG_MODE is defined(RELEASE_MODE optimization)
  *
  * @param color: enum color to be converted
  *
@@ -177,20 +172,21 @@ std::string Ansi_To_Tuple(const EDebugColors color) noexcept
 } /* namespace DebugOnly */
 
 /**
- * @brief Print on console with color and time option
+ * @brief Print on console dynamic number of args with color and time option
  *
- * Only called when DEBUG_MODE is defined
- * RELEASE_MODE optimization
+ * The body of the function is only compiled in DEBUG_MODE(RELEASE_MODE optimization)
  *
- * @param ...args: dinamic number of arguments to print regardles of their type
+ * @param ...args: dinamic number of arguments to print regardless of their type
+ * Example usage:
+ * Debug_Log("Loading next level", 69, 420.69);
  *
  * @return void
  */
 template<class... Args>
-inline void Debug_Log(Args&&... args)
+inline void Debug_Log(Args&&... args) noexcept
 {
 #ifdef DEBUG_MODE
-#if (_WIN32_VERSION && _WIN32_VERSION < 10) // windows versions before 10 do not support ANSI
+#if defined (_WIN32_VERSION) && (_WIN32_VERSION < 10) // windows versions before 10 do not support ANSI
     HANDLE _hndl = GetStdHandle(STD_OUTPUT_HANDLE);
     ([&]
     {
@@ -210,21 +206,23 @@ inline void Debug_Log(Args&&... args)
 }
 
 /**
- * @brief Print on console with color and time option
+ * @brief Print on console dynamic number of args with color and time option
  *
- * Only called when DEBUG_MODE is defined
- * RELEASE_MODE optimization
+ * The body of the function is only compiled in DEBUG_MODE(RELEASE_MODE optimization)
  *
  * @param  color: print color
  * @param ...args: dinamic number of arguments to print regardles of their type
  *
+ * Example usage:
+ * Debug_Log(EDebugColors::Red, "Loading next level", 69, 420.69);
+ *
  * @return void
  */
 template<class... Args>
-inline void Debug_Log(const EDebugColors color, Args&&... args)
+inline void Debug_Log(const EDebugColors color, Args&&... args) noexcept
 {
 #ifdef DEBUG_MODE
-#if (_WIN32_VERSION && _WIN32_VERSION < 10) // windows versions before 10 do not support ANSI
+#if defined (_WIN32_VERSION) && (_WIN32_VERSION < 10) // windows versions before 10 do not support ANSI
     HANDLE _hndl = GetStdHandle(STD_OUTPUT_HANDLE);
     ([&]
     {
@@ -244,19 +242,21 @@ inline void Debug_Log(const EDebugColors color, Args&&... args)
 }
 
 /**
- * @brief Print on console with color and time option
+ * @brief Print on console dynamic number of args with color and time option
  *
- * Only called when DEBUG_MODE is defined
- * RELEASE_MODE optimization
+ * The body of the function is only compiled in DEBUG_MODE(RELEASE_MODE optimization)
  *
  * @param  color: print color
  * @param  bShowTime: show date and time of function call
  * @param ...args: dinamic number of arguments to print regardles of their type
  *
+ * Example usage:
+ * Debug_Log(EDebugColors::Red, true, "Loading next level", 69, 420.69);
+ *
  * @return void
  */
 template<class... Args>
-inline void Debug_Log(const EDebugColors color, const bool bShowTime, Args&&... args)
+inline void Debug_Log(const EDebugColors color, const bool bShowTime, Args&&... args) noexcept
 {
 #ifdef DEBUG_MODE
     if(bShowTime)
@@ -265,7 +265,7 @@ inline void Debug_Log(const EDebugColors color, const bool bShowTime, Args&&... 
         auto time_struct = std::chrono::system_clock::to_time_t(call_time);
         std::cout << std::ctime(&time_struct);
     }
-#if (_WIN32_VERSION && _WIN32_VERSION < 10) // windows versions before 10 do not support ANSI
+#if defined (_WIN32_VERSION) && (_WIN32_VERSION < 10) // windows versions before 10 do not support ANSI
     HANDLE _hndl = GetStdHandle(STD_OUTPUT_HANDLE);
     ([&]
     {
---
+++ src/engine/main.cpp
@@ -0,0 +1,8 @@
+#include <debug_mode_definitions.h>
+#include <debug_logger_component.h>
+
+int main(int argc, char* argv[])
+{
+    Debug_Log(EDebugColors::Magenta, "Starting Cherry Engine", ":)");
+    return 0;
+}
