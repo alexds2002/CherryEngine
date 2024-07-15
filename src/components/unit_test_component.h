40cb9f6c81ec43ce7d19e273520d64f706f7b25b
alexds2002 <alexds2002@gmail.com>
2024-07-15 15:52:51 +0300

    [Unit Test] Add simple unit test system

--- src/components/unit_test_component.h
+++ src/components/unit_test_component.h
@@ -1,4 +1,25 @@
 #pragma once
 
-/*TODO(Alex): make a class that can easily benchmark and unit test new and existing code */
+#include <chrono>
+#include <debug_logger_component.h>
 
+class UnitTest
+{
+public:
+    UnitTest() noexcept;
+    ~UnitTest() noexcept;
+private:
+    std::chrono::time_point<std::chrono::steady_clock> m_begin_time;
+};
+
+inline UnitTest::UnitTest() noexcept
+{
+    m_begin_time = std::chrono::steady_clock::now();
+}
+
+inline UnitTest::~UnitTest() noexcept
+{
+    auto end_time = std::chrono::steady_clock::now();
+    auto time_passed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - m_begin_time);
+    Debug_Log(EDebugColors::LightRed, "Time passed is: ", time_passed.count(), "ms");
+}
