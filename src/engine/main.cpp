2f575796257f568b69fb59b7e5c789eab9c9752f
alexds2002 <alexds2002@gmail.com>
2024-07-19 15:10:38 +0300

    [Clean up] clean up main.cpp

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -4,32 +4,8 @@
 #include <release_logger_component.h>
 #include <benchmark_component.h>
 
-struct Test
-{
-    int64_t test;
-};
-
-void dummpy()
-{
-    int64_t test11;
-    printStackPointer();
-}
-
 int main(int argc, char* argv[])
 {
-    CHERRY_ASSERT(1 == 2, "HELLO");
-    for(int i = 1; i < 17; ++i)
-    {
-        BenchMark ut;
-        ThreadPool thread_pool(i);
-        for(int i = 0; i < 10000; ++i)
-        {
-            thread_pool.Add_Task(TestFunction);
-        }
-    }
-    printStackPointer();
-    dummpy();
-    printStackPointer();
     Debug_Log(EPrintColor::Magenta, "Starting Cherry Engine...");
     return EXIT_SUCCESS;
 }
