121809d4afce0e80153041633afddee8e0fe777f
alexds2002 <alexds2002@gmail.com>
2024-09-25 19:19:56 +0300

    Add the glad submodule
    TODO: add the generated files from the python script in the submodule

--- core/Application.cpp
+++ core/Application.cpp
@@ -1,40 +1,88 @@
 #include "Application.h"
 #include <debug_logger_component.h>
-#include <debug_mode_definitions.h>
-#include <debug_assert_component.h>
-#include <release_logger_component.h>
-#include <benchmark_component.h>
-#include <heap_memory_track_component.h>
-#include <stack_memory_track_component.h>
-#include <project_definitions.h>
 
+#include <iostream>
+
+// GLAD
+#include <glad/gl.h>
+
+// GLFW (include after glad)
 #include <GLFW/glfw3.h>
+
+
+// This example is taken from http://learnopengl.com/
+// http://learnopengl.com/code_viewer.php?code=getting-started/hellowindow2
+// The code originally used GLEW, I replaced it with Glad
+
+// Compile:
+// g++ example/c++/hellowindow2.cpp -Ibuild/include build/src/gl.c -lglfw -ldl
+
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
 
 bool Application::Init()
 {
     Debug_Log(ELogCategory::Default, EPrintColor::Magenta, "Starting Cherry Engine...");
-    GLFWwindow* window;
 
-    /* Init GLFW */
-    if( !glfwInit() )
-       exit( EXIT_FAILURE );
+    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+    // Init GLFW
+    glfwInit();
+    // Set all the required options for GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    window = glfwCreateWindow( 400, 400, "Boing (classic Amiga demo)", NULL, NULL );
-    if (!window)
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (window == NULL)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        exit( EXIT_FAILURE );
+        return -1;
     }
-    while(!glfwWindowShouldClose(window))
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+
+    // Load OpenGL functions, gladLoadGL returns the loaded version, 0 on error.
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0)
     {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
     }
 
-    glfwTerminate();
-    exit( EXIT_SUCCESS );
+    // Successfully loaded OpenGL
+    std::cout << "Loaded OpenGL " << GLAD_VERSION_MAJOR(version) << "." << GLAD_VERSION_MINOR(version) << std::endl;
 
-    return true; /* success */
+    // Define the viewport dimensions
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Terminates GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+    return 0;
+
 }
 
 void Application::Update(double deltaTime)
@@ -42,4 +90,11 @@ void Application::Update(double deltaTime)
     while(true)
     {
     }
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
