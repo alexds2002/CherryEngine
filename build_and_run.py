ed6eb8a7242b6c458fb3ecde0417d6ade92b14a7
alexds2002 <alexds2002@gmail.com>
2024-10-31 13:19:11 +0200

    Update build script

--- build_and_run.py
+++ build_and_run.py
@@ -1,5 +1,10 @@
 import os
 
-os.system('mkdir build')
-os.chdir('./build')
-os.system('cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release .. && make -j4 && ./CherrY')
+def main():
+    os.system('mkdir build')
+    os.chdir('./build')
+    # Generates unix files that can be run from the Terminal
+    os.system('cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release .. && make -j4 && ./CherrY')
+
+if name == "__main__":
+    main()
