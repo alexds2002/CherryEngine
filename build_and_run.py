08f39aefd8f1a5617b86353db9af3ca54d39a233
alexds2002 <alexds2002@gmail.com>
2024-11-01 10:46:16 +0200

    [CMake] Supress the Unused variable warning since it is common to have multiple parameters in a function and not use all of them

--- build_and_run.py
+++ build_and_run.py
@@ -3,7 +3,7 @@ import os
 def main():
     os.system('mkdir build')
     os.chdir('./build')
-    # Generates unix files that can be run from the Terminal
+    # Generates unix make files instead of the Windows equivelent
     os.system('cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release .. && make -j4 && ./CherrY')
 
 if name == "__main__":
